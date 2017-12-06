bb2b0234c5aa96049e81b5f2f172823d66afd13c
bhdwivedi <calthati@gmail.com>
2017-12-06 15:11:53 -0500

    Initial code for spam detector using sklearn multinomialNB

---
+++ spam_detector.py
@@ -0,0 +1,20 @@
+from sklearn.naive_bayes import MultinomialNB
+import pandas as pd 
+import numpy as np 
+
+data = pd.read_csv('../data/spambase.data').as_matrix()
+np.random.shuffle(data)
+
+X = data[:, :48]
+y = data[:, -1]
+
+X_train = X[:-100,]
+y_train = y[:-100,]
+X_test = X[-100:,]
+y_test = y[-100:,]
+
+
+clf = MultinomialNB()
+clf.fit(X_train, y_train)
+s = clf.score(X_test, y_test)
+print("score: ", s)
